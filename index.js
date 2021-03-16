8b1444e22d63fa5edb8bae0df6124db1c4e65393
JJKOrlino <jjkorlino80@yahoo.com>
2021-03-16 17:03:01 -0500

    commit tree f03d6a8f1dd94fb97acc7f2f785e575b96ee4402

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 62
+const num2 = 1
 
-// Write your code here
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 100) + 1
+
+const num3 = 40
+const num4 = 10
+
+const mod = num3 / num4
+
+const max = (2,3,10,19,20)
